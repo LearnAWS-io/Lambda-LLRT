6a1bac4c9385a51f70971b4beee3b7bff9c9e09b
Shivam <singhshivamkr@gmail.com>
2024-03-07 14:06:59 +0530

    :tada: add LLRT and NodeJS function

--- lib/lambda-llrt-stack.ts
+++ lib/lambda-llrt-stack.ts
@@ -1,16 +1,50 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { NodejsFunction, OutputFormat } from "aws-cdk-lib/aws-lambda-nodejs";
+import { CfnOutput, Stack, StackProps } from "aws-cdk-lib";
+import { PolicyStatement } from "aws-cdk-lib/aws-iam";
+import { LlrtFunction } from "cdk-lambda-llrt";
+import { Construct } from "constructs";
+import {
+  Architecture,
+  FunctionUrlAuthType,
+  Runtime,
+} from "aws-cdk-lib/aws-lambda";
 
-export class LambdaLlrtStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class LambdaLlrtStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const llrtFn = new LlrtFunction(this, "llrt-demo-fn", {
+      entry: "lambda/index.ts",
+      architecture: Architecture.ARM_64,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'LambdaLlrtQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const nodejsFn = new NodejsFunction(this, "nodejs-demo-fn", {
+      entry: "lambda/index.ts",
+      architecture: Architecture.ARM_64,
+      runtime: Runtime.NODEJS_20_X,
+      bundling: {
+        format: OutputFormat.ESM,
+        minify: true,
+      },
+    });
+
+    const s3Policy = new PolicyStatement({
+      actions: ["s3:ListAllMyBuckets"],
+      resources: ["*"],
+    });
+
+    llrtFn.addToRolePolicy(s3Policy);
+    nodejsFn.addToRolePolicy(s3Policy);
+
+    const llrtFnUrl = llrtFn.addFunctionUrl({
+      authType: FunctionUrlAuthType.NONE,
+    });
+
+    const nodejsFnUrl = nodejsFn.addFunctionUrl({
+      authType: FunctionUrlAuthType.NONE,
+    });
+
+    new CfnOutput(this, "llrt-url", { value: llrtFnUrl.url });
+    new CfnOutput(this, "nodejs-url", { value: nodejsFnUrl.url });
   }
 }
